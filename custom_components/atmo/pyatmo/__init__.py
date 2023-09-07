3d583d21ffd71a95bf08ed931cbfbf0e04d6dccb
Nathan Spencer <natekspencer@gmail.com>
2023-09-07 01:44:19 +0000

    Update headers

--- custom_components/atmo/pyatmo/__init__.py
+++ custom_components/atmo/pyatmo/__init__.py
@@ -424,8 +424,9 @@ class AtmoBluetoothDeviceData(BluetoothData, AtmoDataMixin):
             if None in data.values():
                 return  # not ready to update
             url = "https://sensorsws.planetwatch.io/atmotube/v1"
+            headers = {"Content-Type": "text/plain"}
             try:
-                async with self.client.post(url, json=data) as resp:
+                async with self.client.post(url, json=data, headers=headers) as resp:
                     resp.raise_for_status()
                     _LOGGER.debug("%s: %s %s", address, resp.status, await resp.text())
                     self.update_sensor(
